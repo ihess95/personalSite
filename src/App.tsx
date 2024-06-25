852ddf1c4aebe62fa623421889b4cfc7eab52f49
Ian Hess <ianhess@Ians-MacBook-Pro.local>
2024-06-25 14:05:06 -0400

    First real commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div className="body">
+      <Header />
+      <h1 className="text-3xl font-bold underline bg-green-50">Hello world!</h1>
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="header flex justify-between">
+      <div className="title">Hess Dev</div>
+      <nav>
+        <a href="https://www.google.com">Google</a>
+      </nav>
+    </div>
+  );
+}
+export default Header;
